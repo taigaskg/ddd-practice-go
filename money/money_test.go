b4dbb990b4fbe17d79329719cf3d3e7fc6d13664
TaigaSakaguchi <gucci.tennismath@gmail.com>
2022-04-07 16:15:36 +0900

    fix add method for money struct

--- money/money_test.go
+++ money/money_test.go
@@ -1,7 +1,6 @@
 package money
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -37,8 +36,30 @@ func TestNew(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	m := Money{10, "a"}
-	// n := Money{12, "aa"}
-	mm, err := m.Add(&Money{})
-	fmt.Println(mm, err)
+	cases := map[string]struct {
+		m1             Money
+		m2             Money
+		expectedAmount uint32
+		isError        bool
+	}{
+		"same currency":      {Money{10, "jpy"}, Money{20, "jpy"}, 30, false},
+		"different currency": {Money{10, "jpy"}, Money{20, "usd"}, 0, true},
+	}
+
+	for k, v := range cases {
+		name := k
+		tc := v
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			m, err := tc.m1.Add(&tc.m2)
+			if tc.isError {
+				assert.Nil(t, m)
+				assert.Error(t, err)
+			} else {
+				assert.NotNil(t, m)
+				assert.Equal(t, tc.expectedAmount, m.Amount)
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
