f4a1c3ec7f33dd5056e0d1b1ef64be5623e61d96
TaigaSakaguchi <gucci.tennismath@gmail.com>
2022-04-16 16:08:13 +0900

    create model number value object

---
+++ modelnumber/modelnumber.go
@@ -0,0 +1,29 @@
+package modelnumber
+
+import (
+	"errors"
+	"fmt"
+)
+
+type ModelNumber struct {
+	productCode string
+	branch      string
+	lot         string
+}
+
+func New(productCode, branch, lot string) (*ModelNumber, error) {
+	if productCode == "" {
+		return nil, errors.New("productCode must not be empty")
+	}
+	if branch == "" {
+		return nil, errors.New("branch must not be empty")
+	}
+	if lot == "" {
+		return nil, errors.New("lot must not be empty")
+	}
+	return &ModelNumber{productCode, branch, lot}, nil
+}
+
+func (mn *ModelNumber) ToString() string {
+	return fmt.Sprintf("%s-%s-%s", mn.productCode, mn.branch, mn.lot)
+}
